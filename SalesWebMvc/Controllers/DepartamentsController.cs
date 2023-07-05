573b0869869228689cf7946049caff694fb8787d
Andressa Santos <andressavieiradsantos@gmail.com>
2023-07-05 18:10:24 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament (1, "Electronics"));
+            list.Add(new Departament (2, "Fashion"));
+
+            return View(list);
+        }
+    }
+}
